3952b2d1cdaf0114807797cc2b16d37332cb85d0
flash76 <30060599+flash76@users.noreply.github.com>
2022-06-18 16:21:22 -0400

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div>
+      <p class="text-2xl text-[#EFAE04]">Chief2</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+/* @refresh reload */
+import './index.css';
+import { render } from 'solid-js/web';
+import { Component } from 'solid-js';
+
+const Index: Component = () => {
+  return (
+    <div class="">
+    </div>
+  );
+};
+
+render(() => <Index />, document.getElementById('root') as HTMLElement);
