2f6c9993803429422464438dfeca8f57b44383bd
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-19 16:59:41 -0400

    basic qrcode scanner

--- backend/DataReader.py
+++ backend/DataReader.py
@@ -1,3 +1,26 @@
+import cv2
+from pyzbar.pyzbar import decode
+
+
 class DataReader:
-    def __init__():
-        pass
+    def __init__(self):
+        self.cap = cv2.VideoCapture(0)
+
+    def _decoder(self, image):
+        gray_img = cv2.cvtColor(image, 0)
+        barcode = decode(gray_img)
+
+        for obj in barcode:
+            barcode_data = obj.data.decode("utf-8")
+            return barcode_data
+
+    def read_qrcode(self):
+        result = None
+
+        while not result:
+            ret, frame = self.cap.read()
+            result = self._decoder(frame)
+            cv2.imshow('Image', frame)
+            cv2.waitKey(10)
+
+        return result
