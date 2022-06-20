c4895c608538191ca1acae0cea201cd9acab5e99
Shom770 <82843611+Shom770@users.noreply.github.com>
2022-06-20 16:55:19 -0400

    csv to json working now.

--- backend/DataReader.py
+++ backend/DataReader.py
@@ -1,9 +1,26 @@
+from io import StringIO
+
 import cv2
 import numpy as np
+import pandas as pd
 from pyzbar.pyzbar import decode
 
 
 class DataReader:
+    _HEADERS_CSV = (
+        "Scout ID,Match Key,Team Number,Alliance,Driver Station,Preloaded Cargo,Auto Lower Hub,Auto Upper Hub,"
+        "Auto Misses,Taxied,Auto Shooting Zones,Teleop Lower Hub,Teleop Upper Hub,Teleop Misses,Teleop Shooting Zones,"
+        "Low Attempted,Mid Attempted,High Attempted,Traversal Attempted,Climb Time,Final Climb Type,"
+        "How much do they play defense?,Defense Rating,"
+        "How much were they playing through defense?,Counter Defense Rating,Driver Rating"
+    )
+    _HEADERS_JSON = (
+        "scout_id,match_key,team_number,alliance,driver_station,preloaded_cargo,auto_lower_hub,auto_upper_hub,"
+        "auto_misses,taxied,auto_shooting_zones,teleop_lower_hub,teleop_upper_hub,teleop_misses,"
+        "teleop_shooting_zones,attempted_low,attempted_mid,attempted_high,attempted_traversal,climb_time,"
+        "final_climb_type,defense_pct,counter_defense_pct,defense_rating,counter_defense_rating,driver_rating"
+    )
+
     def __init__(self, cam):
         self.cap = cv2.VideoCapture(cam)
 
@@ -17,9 +34,13 @@ class DataReader:
             pts = pts.reshape((-1, 1, 2))
             cv2.polylines(image, [pts], True, (0, 255, 0), 3)
 
-            barcode_data = obj.data.decode("utf-8")
+            barcode_data = self._HEADERS_CSV + "\n" + obj.data.decode("utf-8")
 
-            return barcode_data
+            csv_data = pd.read_csv(StringIO(barcode_data))
+            return {
+                key: csv_data[corresponding_header][0]
+                for key, corresponding_header in zip(self._HEADERS_JSON.split(","), self._HEADERS_CSV.split(","))
+            }
 
     def read_qrcode(self):
         result = None
