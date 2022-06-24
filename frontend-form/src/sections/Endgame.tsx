394924998431d6109fad27e20194bc3397edba29
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-06-24 01:47:36 -0400

    Final endgame Ui
    all css removed and properly formatted to what figma looked like. used widget from auto-teleop.
    
    Co-Authored-By: Agneya <72951155+Vort3xed@users.noreply.github.com>

--- frontend-form/src/sections/Endgame.tsx
+++ frontend-form/src/sections/Endgame.tsx
@@ -1,13 +1,20 @@
-import { Component, createSignal } from "solid-js";
+import { Component, createSignal, For } from "solid-js";
+import { ToggleButton } from "./ToggleButton";
 
 
 export const Endgame: Component = () => {
 
   const [time, setTime] = createSignal(0);
-
-  const timer = setInterval(() => {setTime(time() + 1)}, 1000);
-
+  var cleared = false;
+  var timer = setInterval(() => {setTime(time() + 1)}, 1000);
   
+
+  const [rungs, setRungs] = createSignal([
+    { text: "Low", id: "LowToggle"},
+    { text: "Medium", id: "MediumToggle"},
+    { text: "High", id: "HighToggle"},
+    { text: "Traversal", id: "TraversalToggle"}
+  ])
 
   return (
     <div class="items-middle justify-center align-center">
@@ -15,66 +22,72 @@ export const Endgame: Component = () => {
         <p class="font text-center text-3xl ">Endgame</p>
         <div class="m-4" style="text-align:center">
 
-          <p class="font-bold" style="display: inline;">Timer: &nbsp </p>
-          <p style="display: inline;">{Math.floor(time() / 60)}:{time() % 60}</p>
+        <p class="font-bold">Timer</p>
+          <p class="inline">{Math.floor(time() / 60)}:{(time() % 60).toString().padStart(2, "0")}</p>
 
         </div>
 
-        <div class="m-4" style="text-align:center">
-          <button id="reset-timer" style="border: 2px solid black;display: inline;padding: 5px;width: 200px;height: 50px;border-radius: 15px;background: #A1A1A1;">Reset Timer</button>
-          <p style="display: inline">&nbsp&nbsp&nbsp</p>
-          <button id="start-stop"style="border: 2px solid black;display: inline;padding: 5px;width: 200px;height: 50px;border-radius: 15px;background: #A1A1A1;">Start/Stop</button>  
+        <div class="grid grid-cols-2 mb-4">
+          <button id="reset-timer" class="inline col-span-1 h-12 bg-[#d9d9d9] mr-2" onClick={() => {
+            setTime(0)
+            clearInterval(timer)
+            cleared = true
+          }}>
+            Reset Timer
+          </button>
+          <button id="reset-timer" class="inline col-span-1 h-12 bg-[#d9d9d9] mr-2" onClick={() => {
+            if (!cleared){
+              cleared = true
+              clearInterval(timer)
+            }
+            else {
+              cleared = false
+              timer = setInterval(() => {setTime(time() + 1)}, 1000);
+            }
+          }}>
+            Toggle Timer
+          </button>
         </div>
 
-        <div style="text-align:center">
-          <p style="font-size:20px">Attempted Climb</p>
-          <p>&nbsp</p>
-          <button id="low-climb" style="border: 2px solid black;display: inline;padding: 5px;width: 100%;height: 50px;border-radius: 25px;background: #A1A1A1;">Low</button>
-          <p style="line-height:0.5;">&nbsp</p>
-          <button id="medium-climb" style="border: 2px solid black;display: inline;padding: 5px;width: 100%;height: 50px;border-radius: 25px;background: #A1A1A1;">Medium</button>
-          <p style="line-height:0.5;">&nbsp</p>
-          <button id="high-climb" style="border: 2px solid black;display: inline;padding: 5px;width: 100%;height: 50px;border-radius: 25px;background: #A1A1A1;">High</button>
-          <p style="line-height:0.5;">&nbsp</p>
-          <button id="traversal-climb" style="border: 2px solid black;display: inline;padding: 5px;width: 100%;height: 50px;border-radius: 25px;background: #A1A1A1;">Traversal</button>
+        <div class="text-center mt-4">
+          <p class="font-bold">Attempted Climb</p>
+          <For each={rungs()}>{(rung, i) => 
+              <ToggleButton text={rung.text} id={rung.id} />
+          }</For>
         </div>
 
-        <div style="text-align:center">
-          <p style="line-height:2;">&nbsp</p>
-          <p style="font-size:20px;display: inline;">Final Climb Time (In seconds):&nbsp</p>
-
-          <input
-            type="number"
-            id="final-time"
-            style="display: inline;border: 2px solid black;border-radius: 5px;"
-          />
+        <div style="text-align:center" class="mt-4">
+          <p class="font-bold">Final Stats</p>
+          <div class="grid grid-cols-5 my-4">
+            <p class="col-span-2 float-left">Final Time:</p>
+            <input
+              type="number"
+              id="final-time"
+              class="col-span-3 inline border-2 border-[#7b7b7b] rounded-md"
+            />
+          </div>
+          <div class="grid grid-cols-5 my-4">
+            <p class="col-span-2 float-left">Final Time:</p>
+            <fieldset class="col-span-3 flex flex-col">
+              <label class="w-full mx-auto flex-1 bg-gray-200 mx-1 rounded-lg py-1 mb-1">
+                <input class="float-left ml-2" type="radio" id="low" name="climb" value="low"/>
+                <label for="low" class="float-left ml-4 mt-1 inline">Low</label>
+              </label>
+              <label class="w-full flex-1 bg-gray-200 mx-1 rounded-lg py-1 mb-1">
+                <input class="float-left ml-2" type="radio" id="low" name="climb" value="medium"/>
+                <label for="meidum" class="float-left ml-4 mt-1 inline">Medium</label>
+              </label>
+              <label class="w-full flex-1 bg-gray-200 mx-1 rounded-lg py-1 mb-1">
+                <input class="float-left ml-2" type="radio" id="low" name="climb" value="high"/>
+                <label for="high" class="float-left ml-4 mt-1 inline">High</label>
+              </label>
+              <label class="w-full flex-1 bg-gray-200 mx-1 rounded-lg py-1 mb-1">
+                <input class="float-left ml-2" type="radio" id="low" name="climb" value="traversal"/>
+                <label for="traversal" class="float-left ml-4 mt-1 inline">Traversal</label>
+              </label>
+            </fieldset>
+          </div>
         </div>
-
-        <p style="color:white">agneya was here</p>
-
-        <div style="text-align:center">
-          <p style="font-size:20px;display: inline;">Final Climb Type:&nbsp</p>
-
-          <fieldset class="flex flex-row">
-            <label class="flex-1 bg-gray-200 mx-1 rounded-xl px-2 py-1">
-              <input style="text-align:center" type="radio" id="low" name="climb" />
-              <label for="low">Low</label>
-            </label>
-            <label class="flex-1 bg-gray-200 mx-1 rounded-xl px-2 py-1">
-              <input style="text-align:center" type="radio" id="medium" name="climb" />
-              <label for="medium">Medium</label>
-            </label>
-            <label class="flex-1 bg-gray-200 mx-1 rounded-xl px-2 py-1">
-              <input style="text-align:center" type="radio" id="high" name="climb" />
-              <label for="high">High</label>
-            </label>
-            <label class="flex-1 bg-gray-200 mx-1 rounded-xl px-2 py-1">
-              <input style="text-align:center" type="radio" id="traversal" name="climb" />
-              <label for="traversal">Traversal</label>
-            </label>
-          </fieldset>
-        </div>
-
-        
       </div>
     </div>
 
