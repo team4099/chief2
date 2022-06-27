3b47cd3366b16a1fae6b5021b0a9f75e8ec71a95
sswadkar <saraanshwadkar@gmail.com>
2022-06-27 00:44:10 -0400

    allow looking through multiple qr codes

--- backend/DataWriter.py
+++ backend/DataWriter.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+from DataReader import DataReader
+
 
 class DataWriter:
     _HEADERS_CSV = (
@@ -11,17 +13,19 @@ class DataWriter:
     )
 
     def write_data(self, year):
-        result_json = {'scout_id': 'pranav', 'match_key': 'qm3', 'team_number': 1719, 'alliance': 'Red', 'driver_station': 2, 'preloaded_cargo': 1, 'auto_lower_hub': 5, 'auto_upper_hub': 5, 'auto_misses': 0, 'taxied': 1, 'auto_shooting_zones': 'Tarmac', 'teleop_lower_hub': None, 'teleop_upper_hub': 1, 'teleop_misses': 5, 'teleop_shooting_zones': 'Elsewhere', 'attempted_low': None, 'attempted_mid': 1, 'attempted_high': None, 'attempted_traversal': None, 'climb_time': 1, 'final_climb_type': 'No Climb', 'defense_pct': None, 'counter_defense_pct': None, 'defense_rating': None, 'counter_defense_rating': None, 'driver_rating': None}
-        csv_formatted = ",".join([str(value or "") for value in result_json.values()])
+        result_json = DataReader(0).read_qrcode()
 
-        try:
-            with open(f"{year}_{result_json['match_key']}.csv", "r+") as file:
-                if csv_formatted not in file.read():
+        for json in result_json:
+            csv_formatted = ",".join([str(value or "") for value in json.values()])
+
+            try:
+                with open(f"{year}_{json['match_key']}.csv", "r+") as file:
+                    if csv_formatted not in file.read():
+                        file.write(csv_formatted + "\n")
+            except FileNotFoundError:
+                with open(f"{year}_{json['match_key']}.csv", "w") as file:
+                    file.write(self._HEADERS_CSV + "\n")
                     file.write(csv_formatted + "\n")
-        except FileNotFoundError:
-            with open(f"{year}_{result_json['match_key']}.csv", "w") as file:
-                file.write(self._HEADERS_CSV + "\n")
-                file.write(csv_formatted + "\n")
 
 
 DataWriter().write_data(2022)
