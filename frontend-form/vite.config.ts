5153f1e6965def73768995ef76a08b2a92244fa7
flash76 <30060599+flash76@users.noreply.github.com>
2022-06-28 10:15:08 -0400

    add PWA dependencies and config

--- frontend-form/vite.config.ts
+++ frontend-form/vite.config.ts
@@ -1,8 +1,35 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import {VitePWA} from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [solidPlugin(), VitePWA({
+      includeAssets: ['favicon.svg', 'favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Name of your app',
+        short_name: 'Short name of your app',
+        description: 'Description of your app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          }
+        ]
+      }
+    })],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
