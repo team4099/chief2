8058aaab560b31208ffd10361a0b4da1e961f45a
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-07-08 13:20:28 -0400

    fixed qrcode undefined issues

--- frontend-form/src/util/export.ts
+++ frontend-form/src/util/export.ts
@@ -54,10 +54,13 @@ const { defenseTime, defensePlay, defendedTime, defenseCounter, driverRating } =
 const { setQRData } = qrModalState;
 
 export default async function exportQR() {
+
+  const data_points = 29
+
   // The CSV format is like this:
-  // scout_id,match_key,team_number,alliance,driver_station,preloaded_cargo,auto_lower_hub,auto_upper_hub,auto_misses,taxied,auto_shooting_zones,teleop_lower_hub,teleop_upper_hub,teleop_misses,teleop_shooting_zones,attempted_low,attempted_mid,attempted_high,attempted_traversal,climb_time,final_climb_type,defense_pct,counter_defense_pct,defense_rating,counter_defense_rating,driver_rating
+  // scout_id,match_key,team_number,alliance,driver_station,preloaded_cargo,auto_lower_hub,auto_upper_hub,auto_misses,taxied,auto_shooting_zones,teleop_lower_hub,teleop_upper_hub,teleop_misses,teleop_shooting_zones,attempted_low,attempted_mid,attempted_high,attempted_traversal,climb_time,final_climb_type,defense_pct,counter_defense_pct,defense_rating,counter_defense_rating,driver_rating,auto_notes,teleop_notes,endgame_notes
   // Generate a CSV string to match this format and replace true with 1 and false with 0
-  const data =
+  var data =
     `${scoutID()},${matchKey()},${teamNumber()},${alliance()},${driverStation()},${cargoPreload()},${autoLower()},${autoUpper()},${autoMissed()},${taxied()},${autoFender()},${autoOpposingFender()},${autoTarmac()},${autoOpposingTarmac()},${autoLaunchpad()},${autoTerminal()},${autoElsewhere()},${teleopLower()},${teleopUpper()},${teleopMissed()},${teleopFender()},${teleopOpposingFender()},${teleopTarmac()},${teleopOpposingTarmac()},${teleopLaunchpad()},${teleopTerminal()},${teleopElsewhere()},${attemptedLow()},${attemptedMedium()},${attemptedHigh()},${attemptedTraversal()},${finalClimbTime()},${finalClimb()},${defenseTime()},${defensePlay()},${defendedTime()},${defenseCounter()},${driverRating()}`
 
       .replace("undefined", "")
@@ -65,6 +68,12 @@ export default async function exportQR() {
       .replace("false", "0");
   data.replace("undefined", "");
 
+  for (var i = 0; i < data_points; i++){
+    data = data.replace("undefined", "");
+    data = data.replace("true", "1");
+    data = data.replace("false", "0");
+  }
+  
   QrCreator.render(
     {
       text: data,
