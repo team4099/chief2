310230977d46529303500622d21db0907320cde2
AlphaPranav9102 <49816864+AlphaPranav9102@users.noreply.github.com>
2022-10-15 16:36:33 -0400

    added gestures to shooting

--- frontend-form/src/components/ShotWidget.tsx
+++ frontend-form/src/components/ShotWidget.tsx
@@ -1,4 +1,4 @@
-import { Accessor, Component, Setter } from "solid-js";
+import { Accessor, Component, createSignal, Setter } from "solid-js";
 
 type ShotWidgetProps = {
   getter: Accessor<Number>;
@@ -8,8 +8,32 @@ type ShotWidgetProps = {
 export const ShotWidget: Component = ({ getter, setter }: ShotWidgetProps) => {
   // const [shotCounter, setCounter] = createSignal(0);
 
+  const minSwipeDist = 150;
+
+  const [swipeStartX, setSwipeStartX] = createSignal(0);
+  const [swipeEndX, setSwipeEndX] = createSignal(0);
+
+  function checkDirection() {
+    if (swipeStartX() < swipeEndX() && Math.abs(swipeStartX() - swipeEndX()) > minSwipeDist){
+      console.log("left")
+      setter(getter() - 1);
+    }
+    else if (swipeStartX() > swipeEndX() && Math.abs(swipeStartX() - swipeEndX()) > minSwipeDist){
+      console.log("right")
+      setter(getter() + 1);
+    }
+  }
+
   return (
-    <div class="h-14">
+    <div class="h-14"
+      onTouchStart={(e) => {
+        setSwipeStartX(e.changedTouches[0].screenX);
+      }}
+      onTouchEnd={(e) => {
+        setSwipeEndX(e.changedTouches[0].screenX);
+        checkDirection();
+      }}
+    >
       <button
         type="button"
         class="w-1/3 h-full text-3xl rounded-l-xl float-left bg-[#e5534b]"
